f28b18654ab94184cb37fab41e5b7bd67e72ce47
chenjun <398955404@qq.com>
2018-01-14 11:50:41 +0800

    merge

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,33 @@
 var express = require("express");
-var multer = require("multer");
-var upload = multer({ dest: "uploads/" });
-
 var router = express.Router();
+var upload = require('./fileuploads');
+
+var Articles = require('../models/articles');
+
+router.post("/upload", upload.single('file'), function (req, res) {
+  console.log(req.file)
+  res.json({ code: 0, sucessed: true, data: { link: `http://localhost:3000/uploads/${req.file.filename}` } })
+});
+
+router.post("/api/article/add", function (req, res) {
+  // Articles.created(req.body, function (err, data) {
+
+  // })
+  new Articles(req.body).save(function (err) {
+    console.log(err)
+  })
+  console.log(req.body)
+  res.send(200)
+})
+
+router.get("/api/article/get", function (req, res) {
+  Articles.find({}, function (err, result) {
+    res.json(result)
+  })
+})
 
 /* GET home page. */
-router.get("/", function(req, res, next) {
+router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
 });
-
-router.post("/upload", upload.single("avatar"), function(req, res) {
-  res.header("Access-Control-Allow-Origin", "*");
-  console.log(req.files);
-  console.log(req.body);
-  res.send(200);
-});
-
-router.options("/upload", function(req, res) {
-  res.send(200);
-});
-
 module.exports = router;
