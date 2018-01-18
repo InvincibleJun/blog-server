af0af6feae47e3914441592cc8b084c32597ac50
chenjun <398955404@qq.com>
2018-01-18 20:53:38 +0800

    merge

--- controllers/draft.js
+++ controllers/draft.js
@@ -14,7 +14,7 @@ async function upload(req, res) {
   const multer = require("multer");
   const storage = multer.diskStorage({
     destination: function(req, file, cb) {
-      cb(null, "./public/uploads");
+      cb(null, "../public/uploads");
     },
     filename: function(req, file, cb) {
       var fileFormat = file.originalname.split(".");
